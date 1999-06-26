d5837798b0033d860402779fcc5934e6158648fa
Dieter Baron <dillo@nih.at>
1999-06-26 00:13:32 +0000

    *** empty log message ***

--- zip.c
+++ zip.c
@@ -1,11 +1,13 @@
 #include "zip.h"
 
+static int zip_data_unchange(struct zipfile *zf, int idx);
+
 
 
 int
 zip_rename(struct zipfile *zf, int idx, char *name)
 {
-    if (idx >= zf->nentry)
+    if (idx >= zf->nentry || idx < 0)
 	return -1;
 
     if (zf->entry[idx].state == Z_UNCHANGED) 
@@ -46,7 +48,7 @@ zip_add_file(struct zipfile *zf, char *name, FILE *file,
 
 
 int
-zip_add_buf(struct zipfile *zf, char *name, char *buf,
+zip_add_data(struct zipfile *zf, char *name, char *buf,
 	int start, int len)
 {
     if (zf->nentry >= zf->entry_size-1) {
@@ -73,18 +75,19 @@ int
 zip_replace_file(struct zipfile *zf, int idx, char *name, FILE *file,
 	    int start, int len)
 {
-    if (idx >= zf->nentry)
+    if (idx >= zf->nentry || idx < 0)
 	return -1;
 
+    zip_unchange_data(zf, idx)
+
     zf->entry[idx].state = Z_REPLACED;
-    if (zf->entry[idx].ch_name)
-	free(zf->entry[idx].ch_name);
-    if (name)
+    if (name) {
+	if (zf->entry[idx].ch_name)
+	    free(zf->entry[idx].ch_name);
 	zf->entry[idx].ch_name = strdup(name);
-    else
-	zf->entry[idx].ch_name = NULL;
+    }
+    
     zf->entry[idx].ch_data_fp = file;
-    zf->entry[idx].ch_data_buf = NULL;
     zf->entry[idx].ch_data_offset = start;
     zf->entry[idx].ch_data_len = len;
 
@@ -97,17 +100,17 @@ int
 zip_replace_data(struct zipfile *zf, int idx, char *name, char *buf,
 	    int start, int len)
 {
-    if (idx >= zf->nentry)
+    if (idx >= zf->nentry || idx < 0)
 	return -1;
 
+    zip_unchange_data(zf, idx)
+
     zf->entry[idx].state = Z_REPLACED;
-    if (zf->entry[idx].ch_name)
-	free(zf->entry[idx].ch_name);
-    if (name)
+    if (name) {
+	if (zf->entry[idx].ch_name)
+	    free(zf->entry[idx].ch_name);
 	zf->entry[idx].ch_name = strdup(name);
-    else
-	zf->entry[idx].ch_name = NULL;
-    zf->entry[idx].ch_data_fp = NULL;
+    }
     zf->entry[idx].ch_data_buf = buf;
     zf->entry[idx].ch_data_offset = start;
     zf->entry[idx].ch_data_len = len;
@@ -120,13 +123,10 @@ zip_replace_data(struct zipfile *zf, int idx, char *name, char *buf,
 int
 zip_delete(struct zipfile *zf, int idx)
 {
-    if (idx >= zf->nentry)
+    if (idx >= zf->nentry || idx < 0)
 	return -1;
 
-    if (zf->entry[idx].ch_name) {
-	free(zf->entry[idx].ch_name);
-	zf->entry[idx].ch_name = NULL;
-    }
+    zip_unchange(zf, idx)
 
     zf->entry[idx].state = Z_DELETED;
 
@@ -138,7 +138,7 @@ zip_delete(struct zipfile *zf, int idx)
 int
 zip_unchange(struct zipfile *zf, int idx)
 {
-    if (idx >= zf->nentry)
+    if (idx >= zf->nentry || idx < 0)
 	return -1;
 
     if (zf->entry[idx].ch_name) {
@@ -146,7 +146,31 @@ zip_unchange(struct zipfile *zf, int idx)
 	zf->entry[idx].ch_name = NULL;
     }
 
-    zf->entry[idx].state = Z_UNCHANGED;
+    return zip_data_unchange(zf, idx);
+}
+
+
+static int
+zip_data_unchange(struct zipfile *zf, int idx)
+{
+    if (idx >= zf->nentry || idx < 0)
+	return -1;
+
+    if (zf->entry[idx].ch_data_fp) {
+	fclose(zf->entry[idx].ch_data_fp);
+	zf->entry[idx].ch_data_fp = NULL;
+    }
+    if (zf->entry[idx].ch_data_buf) {
+	free(zf->entry[idx].ch_data_buf);
+	zf->entry[idx].ch_data_buf = NULL;
+    }
+
+    zf->entry[idx].ch_data_zf = NULL;
+    zf->entry[idx].ch_data_zf_fileno = 0;
+    zf->entry[idx].ch_data_offset = 0;
+    zf->entry[idx].ch_data_len = 0;
+
+    zf->entry[idx].state = zf->entry[idx].name ? Z_RENAMED : Z_UNCHANGED;
 
     return idx;
 }
