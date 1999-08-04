a0a8650aaf4c4e6599847b6aa32d47e52c8d9931
Thomas Klausner <tk@giga.or.at>
1999-08-04 15:26:34 +0000

    getoptified

--- mkmamedb.c
+++ mkmamedb.c
@@ -1,21 +1,79 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "getopt.h"
 #include "types.h"
 #include "dbl.h"
 #include "funcs.h"
 #include "mkmamedb.h"
 #include "error.h"
+#include "config.h"
 
 char *prg;
+char *usage = "Usage: %s [-hV] [-o dbfile] [rominfo-file]\n";
+
+char help_head[] = "mkmamedb (" PACKAGE ") by Dieter Baron and"
+                   " Thomas Klausner\n\n";
+
+char help[] = "\n\
+  -h, --help           display this help message\n\
+  -V, --version        display version number\n\
+  -o, --output DBFILE  write to db DBFILE\n\
+\n\
+Report bugs to <nih@giga.or.at>.\n";
+
+char version_string[] = "mkmamedb (" PACKAGE " " VERSION ")\n\
+Copyright (C) 1999 Dieter Baron and Thomas Klausner\n\
+" PACKAGE " comes with ABSOLUTELY NO WARRANTY, to the extent permitted by law.\n\
+You may redistribute copies of\n\
+" PACKAGE " under the terms of the GNU General Public License.\n\
+For more information about these matters, see the files named COPYING.\n";
+
+#define OPTIONS "hVo:"
+
+struct option options[] = {
+    { "help",          0, 0, 'h' },
+    { "version",       0, 0, 'V' },
+    { "output",        1, 0, 'o' },
+    { NULL,            0, 0, 0 },
+};
 
 int
 main(int argc, char **argv)
 {
     DB *db;
     char *dbname;
-    
+    int dbext;
+    char c;
+
     prg = argv[0];
-    dbname = db_name("mame");
+
+    dbname = "mame";
+    dbext = 1;
+    
+    opterr = 0;
+    while ((c=getopt_long(argc, argv, OPTIONS, options, 0)) != EOF) {
+	switch (c) {
+	case 'h':
+	    fputs(help_head, stdout);
+	    printf(usage, prg);
+	    fputs(help, stdout);
+	    exit(0);
+	case 'V':
+	    fputs(version_string, stdout);
+	    exit(0);
+	case 'o':
+	    dbname = optarg;
+	    dbext = 0;
+	    break;
+    	default:
+	    fprintf(stderr, usage, prg);
+	    exit(1);
+	}
+    }
+
+
+    if (dbext)
+	dbname = db_name(dbname);
 
     remove(dbname);
     db = db_open(dbname, 0, 1);
