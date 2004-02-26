d3aa835aca8389f27a0d409f6ab59d77da395160
Thomas Klausner <tk@giga.or.at>
2004-02-26 01:09:45 +0000

    * superflouos.c (handle_extra_files): check for extra files in 	roms or samples respectively dirs. Remove standalone code.

--- superflouos.c
+++ superflouos.c
@@ -1,8 +1,8 @@
 /*
-  $NiH: superflouos.c,v 1.4 2003/02/23 14:48:05 dillo Exp $
+  $NiH: superflouos.c,v 1.5 2003/03/16 10:21:35 wiz Exp $
 
   superflouos.c -- check for unknown file in rom directories
-  Copyright (C) 1999, 2003 Dieter Baron and Thomas Klausner
+  Copyright (C) 1999, 2003, 2004 Dieter Baron and Thomas Klausner
 
   This file is part of ckmame, a program to check rom sets for MAME.
   The authors can be contacted at <nih@giga.or.at>
@@ -29,14 +29,6 @@
 #include <stdlib.h>
 #include <fnmatch.h>
 
-#include "config.h"
-
-#ifdef HAVE_GETOPT_LONG
-#include <getopt.h>
-#else
-#include "getopt.h"
-#endif
-
 #include "types.h"
 #include "dbh.h"
 #include "util.h"
@@ -45,94 +37,40 @@
 
 
 
-char *prg;
-
-char *usage = "Usage: %s [-hVSwsfbdcFKkUuLlvn] [-D dbfile] [game...]\n";
-
-char help_head[] = "superflouos (" PACKAGE ") by Dieter Baron and Thomas Klausner\n\n";
-
-char help[] = "\n\
-  -h, --help           display this help message\n\
-  -V, --version        display version number\n\
-  -D, --db DBFILE      use mame-db DBFILE\n\
-\n\
-Report bugs to <nih@giga.or.at>.\n";
-
-char version_string[] = "superflouos (" PACKAGE ") " VERSION "\n\
-Copyright (C) 2003 Dieter Baron and Thomas Klausner\n\
-" PACKAGE " comes with ABSOLUTELY NO WARRANTY, to the extent permitted by law.\n\
-You may redistribute copies of\n\
-" PACKAGE " under the terms of the GNU General Public License.\n\
-For more information about these matters, see the files named COPYING.\n";
-
-#define OPTIONS "hVD:"
-
-struct option options[] = {
-    { "help",          0, 0, 'h' },
-    { "version",       0, 0, 'V' },
-    { "db",            1, 0, 'D' },
-    { NULL,            0, 0, 0 },
-};
-
-extern char *rompath[];
-
-
-
 int
-main(int argc, char **argv)
+handle_extra_files(DB *db, char *dbname, int sample)
 {
     FILE *fin;
-    int i;
-    DB *db;
-    char **list, *dbname, b[8192], *p;
-    int c, nlist, dbext, l, zip;
-    
-    prg = argv[0];
+    char b[8192], *p, bo[8192], *po, **list, **lists, **listx;
+    int i, l, nfound;
+    int nlist, nlists, nlistx;
 
-    dbext = 0;
-    dbname = getenv("MAMEDB");
-    if (dbname == NULL) {
-	dbname = "mame";
-	dbext = 1;
-    }
-
-    opterr = 0;
-    while ((c=getopt_long(argc, argv, OPTIONS, options, 0)) != EOF) {
-	switch (c) {
-	case 'h':
-	    fputs(help_head, stdout);
-	    printf(usage, prg);
-	    fputs(help, stdout);
-	    exit(0);
-	case 'V':
-	    fputs(version_string, stdout);
-	    exit(0);
-	case 'D':
-	    dbname = optarg;
-	    dbext = 0;
-	    break;
-
-	default:
-	    fprintf(stderr, usage, prg);
-	    exit(1);
-	}
-    }
-    
-    if ((db=ddb_open(dbname, DDB_READ|DDB_EXT))==NULL) {
-	myerror(ERRSTR, "can't open database `%s'", dbname);
-	exit(1);
-    }
+    nfound = 0;
 
     if ((nlist=r_list(db, "/list", &list)) < 0) {
 	myerror(ERRDEF, "list of games not found in database `%s'", dbname);
 	exit(1);
     }
+    if ((nlists=r_list(db, "/sample_list", &lists)) < 0) {
+	myerror(ERRDEF, "list of samples not found in database `%s'", dbname);
+	exit(1);
+    }
+    if ((nlistx=r_list(db, "/extra_list", &listx)) < 0) {
+	myerror(ERRDEF, "list of extra files not found in database `%s'",
+		dbname);
+	exit(1);
+    }
+
+    if (nlist < 1 || (sample && nlists < 1))
+	return 0;
 
     init_rompath();
 
     p = b;
+    po = bo;
     for (i=0; rompath[i]; i++) {
-	sprintf(b, "ls %s/roms", rompath[i]);
+	/* XXX: use opendir */
+	sprintf(b, "ls %s/%s", rompath[i], sample ? "samples" : "roms");
 	if ((fin=popen(b, "r")) == NULL) {
 	    /* XXX: error */
 	    continue;
@@ -140,16 +78,23 @@ main(int argc, char **argv)
 
 	while (fgets(b, 8192, fin)) {
 	    l = strlen(b);
-	    zip = 0;
 	    if (b[l-1] == '\n')
 		b[--l] = '\0';
+	    strncpy(bo, b, sizeof(bo));
 	    if (l > 4 && (strcmp(b+l-4, ".zip") == 0)) {
-		zip = 1;
 		b[l-4] = '\0';
 	    }
-	    if (bsearch(&p, list, nlist, sizeof(char *),
-			(cmpfunc)strpcasecmp) == NULL)
-		printf("%s/roms/%s%s\n", rompath[i], b, (zip ? ".zip" : ""));
+	    if (bsearch(&p, sample ? lists : list,
+			sample ? nlists : nlist,
+			sizeof(char *),
+			(cmpfunc)strpcasecmp) == NULL
+		&& bsearch(&po, listx, nlistx, sizeof(char *),
+			   (cmpfunc)strpcasecmp) == NULL) {
+		if (nfound++ == 0)
+		    printf("Extra files found:\n");
+		printf("%s/%s/%s\n", rompath[i],
+		       sample ? "samples" : "roms", bo);
+	    }
 	}
 	pclose(fin);
     }
