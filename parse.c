65f5cc4e74670b1b7f998a658d75ef28822c717e
Dieter Baron <dillo@nih.at>
2005-06-12 19:33:57 +0000

    add RCS id and license

--- parse.c
+++ parse.c
@@ -1,3 +1,28 @@
+/*
+  $NiH: ckmame.c,v 1.39 2005/06/12 18:00:59 wiz Exp $
+
+  parse.c -- parser frontend
+  Copyright (C) 1999-2005 Dieter Baron and Thomas Klausner
+
+  This file is part of ckmame, a program to check rom sets for MAME.
+  The authors can be contacted at <nih@giga.or.at>
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License, version 2, as
+  published by the Free Software Foundation.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
