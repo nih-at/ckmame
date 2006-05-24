440b14f2dbba1d52d4aaafa232556c9e015639f6
Dieter Baron <dillo@nih.at>
2006-05-24 11:58:02 +0000

    Add copyright notice.

--- src/globals.h
+++ src/globals.h
@@ -1,6 +1,31 @@
 #ifndef HAD_GLOBALS_H
 #define HAD_GLOBALS_H
 
+/*
+  $NiH$
+
+  globals.h -- declaration of global variables
+  Copyright (C) 1999-2006 Dieter Baron and Thomas Klausner
+
+  This file is part of ckmame, a program to check rom sets for MAME.
+  The authors can be contacted at <nih@giga.or.at>
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License, version 2, as
+  published by the Free Software Foundation.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+
+
 #include "dbl.h"
 #include "delete_list.h"
 #include "map.h"
