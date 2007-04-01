ecd351ce6a4fc3259b45db63a96ec31894f449f0
Thomas Klausner <tk@giga.or.at>
2007-04-01 23:17:06 +0000

    Script that parses ckmame output and produces an update set for the missing files.
    XXX: does not handle chds yet.
    XXX: has hardwired paths

---
+++ contrib/mamediffs.pl
@@ -0,0 +1,38 @@
+#!/usr/pkg/bin/perl
+
+$zipname="mamediffs";
+$linkdir="/archive/roms/dillo";
+$rompath="/archive/roms/mame/roms/";
+
+sub do_game_copy {
+    my $gamename = shift;
+    if (@_) {
+	system("unzip","-Cd",$gamename,"$rompath/$gamename.zip",@_);
+	system("zip","-9rm",$zipname,$gamename);
+    }
+}
+
+while (<>) {
+    if (/^In game (.*):/) {
+	if ($gamename) {
+	    do_game_copy($gamename, @files);
+	    @files=();
+	}
+	$gamename = $1;
+    }
+    elsif (/^rom\s*([^\s]*)/ and not /no good dump: exists/
+	   and not /best bad dump/) {
+	push @files, $1;
+    }
+    elsif (/^game\s*([^\s]*)/) {
+	if ( ! -f "$rompath/$gamename.zip") {
+	    print("$gamename not found\n");
+	    next;
+	}
+	if ( ! -d $linkdir ) {
+	    mkdir($linkdir, 0755);
+	}
+	symlink("$rompath/$gamename.zip", "$linkdir/$gamename.zip");
+    }
+}
+do_game_copy($gamename, @files);
