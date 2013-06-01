f8a702bed575fbd4697c3c48bd39067325cada5c
Dieter Baron <dillo@nih.at>
2013-06-01 13:36:23 +0200

    sizeof(*ptr)

--- src/md5.c
+++ src/md5.c
@@ -151,7 +151,7 @@ void MD5Final(unsigned char digest[16], struct MD5Context *ctx)
     
     if (digest!=NULL)
 	    memcpy(digest, ctx->buf, 16);
-    memset(ctx, 0, sizeof(ctx));	/* In case it's sensitive */
+    memset(ctx, 0, sizeof(*ctx));	/* In case it's sensitive */
 }
 
 /* The four core functions - F1 is optimized somewhat */
