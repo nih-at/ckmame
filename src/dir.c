3b7506c75b70b9ba11f44d732e0ea9d23a748425
Thomas Klausner <tk@giga.or.at>
2014-04-10 23:58:21 +0200

    Copyright police.

--- src/dir.c
+++ src/dir.c
@@ -1,6 +1,6 @@
 /*
   dir.h -- reading a directory
-  Copyright (C) 2005 Dieter Baron and Thomas Klausner
+  Copyright (C) 2005-2014 Dieter Baron and Thomas Klausner
 
   This file is part of ckmame, a program to check rom sets for MAME.
   The authors can be contacted at <ckmame@nih.at>
--- src/globals.c
+++ src/globals.c
@@ -1,6 +1,6 @@
 /*
   globals.c -- definition of global variables
-  Copyright (C) 2013 Dieter Baron and Thomas Klausner
+  Copyright (C) 2013-2014 Dieter Baron and Thomas Klausner
 
   This file is part of ckmame, a program to check rom sets for MAME.
   The authors can be contacted at <ckmame@nih.at>
