cfb46df3afbf5c0f07d20393cb5da93ba2abb9db
Dieter Baron <dillo@nih.at>
2018-09-06 13:12:51 +0200

    Use libxml2 to create XML file.

--- src/output_datafile_xml.c
+++ src/output_datafile_xml.c
@@ -37,6 +37,8 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include <libxml/tree.h>
+
 #include "error.h"
 #include "output.h"
 #include "xmalloc.h"
@@ -45,6 +47,8 @@
 struct output_context_xml {
     output_context_t output;
 
+    xmlDocPtr doc;
+    xmlNodePtr root;
     FILE *f;
     char *fname;
 };
@@ -83,6 +87,11 @@ output_datafile_xml_new(const char *fname, int flags) {
 
     ctx->f = f;
     ctx->fname = xstrdup(fname);
+    ctx->doc = xmlNewDoc((const xmlChar *)"1.0");
+    ctx->doc->encoding = (const xmlChar *)strdup("UTF-8");
+    xmlCreateIntSubset(ctx->doc, (const xmlChar *)"datafile", (const xmlChar *)"-//Logiqx//DTD ROM Management Datafile//EN", (const xmlChar *)"http://www.logiqx.com/Dats/datafile.dtd");
+    ctx->root = xmlNewNode(NULL, (const xmlChar *)"datafile");
+    xmlDocSetRootElement(ctx->doc, ctx->root);
 
     return (output_context_t *)ctx;
 }
@@ -90,26 +99,45 @@ output_datafile_xml_new(const char *fname, int flags) {
 
 static int
 output_datafile_xml_close(output_context_t *out) {
-    output_context_xml_t *ctx;
-    int ret;
+    output_context_xml_t *ctx = (output_context_xml_t *)out;
 
-    ctx = (output_context_xml_t *)out;
-
+    int ret = 0;
     if (ctx->f != NULL) {
-	fputs("</datafile>\n", ctx->f);
+        if (xmlDocFormatDump(ctx->f, ctx->doc, 1) < 0) {
+            ret = -1;
+        }
     }
-    if (ctx->f == stdout) {
-	ret = 0;
-    }
-    else {
-	ret = fclose(ctx->f);
+    if (ctx->f != stdout) {
+	ret |= fclose(ctx->f);
     }
 
+    xmlFreeDoc(ctx->doc);
     free(ctx);
 
     return ret;
 }
 
+static void
+set_attribute(xmlNodePtr node, const char *name, const char *value) {
+    if (value == NULL) {
+        return;
+    }
+    xmlSetProp(node, (const xmlChar *)name, (const xmlChar *)value);
+}
+
+static void
+set_attribute_u64(xmlNodePtr node, const char *name, uint64_t value) {
+    char b[128];
+    snprintf(b, sizeof(b), "%" PRIu64, value);
+    set_attribute(node, name, b);
+}
+
+static void
+set_attribute_hash(xmlNodePtr node, const char *name, int type, hashes_t *hashes) {
+    char hstr[HASHES_SIZE_MAX * 2 + 1];
+    
+    set_attribute(node, name, hash_to_string(hstr, type, hashes));
+}
 
 static int
 output_datafile_xml_game(output_context_t *out, game_t *g) {
@@ -120,24 +148,30 @@ output_datafile_xml_game(output_context_t *out, game_t *g) {
     char *fl;
 
     ctx = (output_context_xml_t *)out;
-
-    fprintf(ctx->f, "\t<game name=\"%s\"", game_name(g));
-    output_cond_print_string(ctx->f, " cloneof=\"", game_cloneof(g, TYPE_ROM, 0), "\"");
-    output_cond_print_string(ctx->f, " sampleof=\"", game_cloneof(g, TYPE_SAMPLE, 0), "\"");
-    fputs(">\n", ctx->f);
+    
+    xmlNodePtr game = xmlNewChild(ctx->root, NULL, (const xmlChar *)"game", NULL);
+    
+    set_attribute(game, "name", game_name(g));
+    set_attribute(game, "cloneof", game_cloneof(g, TYPE_ROM, 0));
+    set_attribute(game, "sampleof", game_cloneof(g, TYPE_SAMPLE, 0));
     /* description is actually required */
-    fprintf(ctx->f, "\t\t<description>%s</description>\n", game_description(g) ? game_description(g) : game_name(g));
+    xmlNewChild(game, NULL, (const xmlChar *)"description", (const xmlChar *)(game_description(g) ? game_description(g) : game_name(g)));
+
     for (i = 0; i < game_num_files(g, TYPE_ROM); i++) {
 	r = game_file(g, TYPE_ROM, i);
+        xmlNodePtr rom = xmlNewChild(game, NULL, (const xmlChar *)"rom", NULL);
+        
+        set_attribute(rom, "name", file_name(r));
+        set_attribute_u64(rom, "size", file_size(r));
+        set_attribute_hash(rom, "crc", HASHES_TYPE_CRC, file_hashes(r));
+        set_attribute_hash(rom, "sha1", HASHES_TYPE_SHA1, file_hashes(r));
+        set_attribute_hash(rom, "md5", HASHES_TYPE_MD5, file_hashes(r));
 
-	fprintf(ctx->f, "\t\t<rom name=\"%s\"", file_name(r));
-	fprintf(ctx->f, " size=\"%" PRIu64 "\"", file_size(r));
-	output_cond_print_hash(ctx->f, " crc=\"", HASHES_TYPE_CRC, file_hashes(r), "\"");
-	output_cond_print_hash(ctx->f, " sha1=\"", HASHES_TYPE_SHA1, file_hashes(r), "\"");
-	output_cond_print_hash(ctx->f, " md5=\"", HASHES_TYPE_MD5, file_hashes(r), "\"");
-	if (file_where(r) != FILE_INZIP)
-	    output_cond_print_string(ctx->f, " merge=\"", file_merge(r) ? file_merge(r) : file_name(r), "\"");
-	switch (file_status(r)) {
+        if (file_where(r) != FILE_INZIP) {
+            set_attribute(rom, "merge", file_merge(r) ? file_merge(r) : file_name(r));
+        }
+
+        switch (file_status(r)) {
 	case STATUS_OK:
 	    fl = NULL;
 	    break;
@@ -148,16 +182,18 @@ output_datafile_xml_game(output_context_t *out, game_t *g) {
 	    fl = "nodump";
 	    break;
 	}
-	output_cond_print_string(ctx->f, " status=\"", fl, "\"");
-	fputs("/>\n", ctx->f);
+        set_attribute(rom, "status", fl);
     }
+
     for (i = 0; i < game_num_disks(g); i++) {
 	d = game_disk(g, i);
+        xmlNodePtr disk = xmlNewChild(game, NULL, (const xmlChar *)"disk", NULL);
 
-	fprintf(ctx->f, "\t\t<disk name=\"%s\"", disk_name(d));
-	output_cond_print_hash(ctx->f, " sha1=\"", HASHES_TYPE_SHA1, disk_hashes(d), "\"");
-	output_cond_print_hash(ctx->f, " md5=\"", HASHES_TYPE_MD5, disk_hashes(d), "\"");
-	switch (disk_status(d)) {
+        set_attribute(disk, "name", disk_name(d));
+        set_attribute_hash(disk, "sha1", HASHES_TYPE_SHA1, disk_hashes(d));
+        set_attribute_hash(disk, "md5", HASHES_TYPE_MD5, disk_hashes(d));
+
+        switch (disk_status(d)) {
 	case STATUS_OK:
 	    fl = NULL;
 	    break;
@@ -168,14 +204,15 @@ output_datafile_xml_game(output_context_t *out, game_t *g) {
 	    fl = "nodump";
 	    break;
 	}
-	output_cond_print_string(ctx->f, " status=\"", fl, "\"");
-	fputs("/>\n", ctx->f);
+        set_attribute(disk, "status", fl);
     }
+    
     for (i = 0; i < game_num_files(g, TYPE_SAMPLE); i++) {
-	r = game_file(g, TYPE_SAMPLE, i);
-	fprintf(ctx->f, "\t\t<sample name=\"%s\"/>\n", file_name(r));
+        r = game_file(g, TYPE_SAMPLE, i);
+        xmlNodePtr sample = xmlNewChild(game, NULL, (const xmlChar *)"sample", NULL);
+        
+        set_attribute(sample, "name", file_name(d));
     }
-    fputs("\t</game>\n\n", ctx->f);
 
     return 0;
 }
@@ -186,18 +223,13 @@ output_datafile_xml_header(output_context_t *out, dat_entry_t *dat) {
     output_context_xml_t *ctx;
 
     ctx = (output_context_xml_t *)out;
-
-    fprintf(ctx->f,
-	    "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
-	    "<!DOCTYPE datafile PUBLIC \"-//Logiqx//DTD ROM Management Datafile//EN\" \"http://www.logiqx.com/Dats/datafile.dtd\">\n"
-	    "<datafile>\n"
-	    "\t<header>\n"
-	    "\t\t<name>%s</name>\n"
-	    "\t\t<description>%s</description>\n"
-	    "\t\t<version>%s</version>\n"
-	    "\t\t<author>automatically generated</author>\n"
-	    "\t</header>\n\n",
-	    dat_entry_name(dat), (dat_entry_description(dat) ? dat_entry_description(dat) : dat_entry_name(dat)), dat_entry_version(dat));
+    
+    xmlNodePtr header = xmlNewChild(ctx->root, NULL, (const xmlChar *)"header", NULL);
+    
+    xmlNewChild(header, NULL, (const xmlChar *)"name", (const xmlChar *)dat_entry_name(dat));
+    xmlNewChild(header, NULL, (const xmlChar *)"description", (const xmlChar *)(dat_entry_description(dat) ? dat_entry_description(dat) : dat_entry_name(dat)));
+    xmlNewChild(header, NULL, (const xmlChar *)"version", (const xmlChar *)dat_entry_version(dat));
+    xmlNewChild(header, NULL, (const xmlChar *)"author", (const xmlChar *)"automatically generated");
 
     return 0;
 }
