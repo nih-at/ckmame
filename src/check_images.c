2d77fe24798d99134632094cb9b7e29113ff95f4
Thomas Klausner <tk@giga.or.at>
2020-09-02 09:53:52 +0200

    Fix file name in comment.

--- src/check_images.c
+++ src/check_images.c
@@ -1,5 +1,5 @@
 /*
-  check_disks.c -- match files against disks
+  check_images.c -- match files against disks
   Copyright (C) 1999-2014 Dieter Baron and Thomas Klausner
 
   This file is part of ckmame, a program to check rom sets for MAME.
