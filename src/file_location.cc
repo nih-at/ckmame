17d02e304f5defe34161f153ba4d85cc5e402110
Thomas Klausner <tk@giga.or.at>
2020-10-01 15:15:42 +0200

    Add cast.

--- src/file_location.cc
+++ src/file_location.cc
@@ -63,7 +63,7 @@ file_location_t *
 file_location_new(const char *name, int idx) {
     file_location_t *e;
 
-    e = xmalloc(sizeof(*e));
+    e = static_cast<file_location_t *>(xmalloc(sizeof(*e)));
 
     e->name = xstrdup(name);
     e->index = idx;
