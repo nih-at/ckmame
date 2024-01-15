55f64eff347670f1c201d474e3256ddeba02a3da
Thomas Klausner <wiz@gatalith.at>
2024-01-15 17:08:00 +0100

    Rename dir+zip tests to .vtest and generate single-type tests

--- regress/convert_to_dir_variant.py
+++ regress/convert_to_dir_variant.py
@@ -7,7 +7,11 @@ import re
 def create_zip_version(name):
     """Create zip version of test case."""
     with open(name, 'r', encoding='utf-8') as input_file:
-        output_name = name[:-5] + '.zip.test'
+        last_slash = name.rfind('/')
+        if last_slash != -1:
+            name = name[last_slash+1:]
+        last_dot = name.rfind('.')
+        output_name = name[:last_dot] + '.zip.test'
         with open(output_name, 'w', encoding='utf-8') as output_file:
             for line in input_file.readlines():
                 if line.startswith('<dir>'):
@@ -21,7 +25,11 @@ def create_dir_version(name):
     """Create dir version of test case."""
     with open(name, 'r', encoding='utf-8') as input_file:
         inline_data = False
-        output_name = name[:-5] + '.dir.test'
+        last_slash = name.rfind('/')
+        if last_slash != -1:
+            name = name[last_slash+1:]
+        last_dot = name.rfind('.')
+        output_name = name[:last_dot] + '.dir.test'
         with open(output_name, 'w', encoding='utf-8') as output_file:
             for line in input_file.readlines():
                 if line.startswith('<zip>'):
@@ -46,12 +54,20 @@ def create_dir_version(name):
                 output_file.write(line)
 
 
-parser = argparse.ArgumentParser(description='Create a dir-variant test from '
-                                 + 'one supporting both types.')
+parser = argparse.ArgumentParser(description='Create a dir- and/or zip-variant test from '
+                                 + 'one supporting both types. An input file of name.test will have name.dir.test and/or name.zip.test as output file.')
+parser.add_argument('--dir', action='store_true', help="convert to directory test case (default)")
+parser.add_argument('--zip', action='store_true', help="convert to zip test case")
 parser.add_argument('input', type=str, help='input test case', nargs='+')
 
 args = parser.parse_args()
 
+# default to '--dir' if no arguments are given
+if not args.dir and not args.zip:
+    args.dir = True
+
 for file_name in args.input:
-    create_zip_version(file_name)
-    create_dir_version(file_name)
+    if args.dir:
+        create_dir_version(file_name)
+    if args.zip:
+        create_zip_version(file_name)
