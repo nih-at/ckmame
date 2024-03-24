29643223f17edfc1e00c69e83375e9e729a649a7
Dieter Baron <dillo@nih.at>
2024-03-24 15:15:48 +0100

    Fix copyright notice.

--- src/Progress.cc
+++ src/Progress.cc
@@ -1,22 +1,24 @@
 /*
-Progress.cc --
+Progress.cc -- progress reporting
+Copyright (C) 2024 Dieter Baron and Thomas Klausner
 
-Copyright (C) Dieter Baron
-
-The authors can be contacted at <accelerate@tpau.group>
+This file is part of ckmame, a program to check rom sets for MAME.
+The authors can be contacted at <ckmame@nih.at>
 
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions
 are met:
-
 1. Redistributions of source code must retain the above copyright
-   notice, this list of conditions and the following disclaimer.
+notice, this list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright
+notice, this list of conditions and the following disclaimer in
+the documentation and/or other materials provided with the
+distribution.
+3. The name of the author may not be used to endorse or promote
+products derived from this software without specific prior
+written permission.
 
-2. The names of the authors may not be used to endorse or promote
-  products derived from this software without specific prior
-  written permission.
-
-THIS SOFTWARE IS PROVIDED BY THE AUTHORS "AS IS" AND ANY EXPRESS
+THIS SOFTWARE IS PROVIDED BY THE AUTHORS ``AS IS'' AND ANY EXPRESS
 OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
 WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
 ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY
